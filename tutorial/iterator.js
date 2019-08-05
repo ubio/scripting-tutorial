b165aa558e9752dd3e311d3c21a16683eb4e4be6
andrei <andrei.marin@ub.io>
2019-08-05 13:01:17 +0300

    Added "iterator" page

---
+++ tutorial/iterator.js
@@ -0,0 +1,35 @@
+function randomCheckboxesNumber() {
+   const randomNumber = Math.ceil(Math.random() * (8 - 4) + 4);
+   console.log(randomNumber);
+   for (i = 0; i < randomNumber; ++i) {
+    console.log(randomNumber);
+    document.querySelector('#checkBoxes').innerHTML += '<input class="checkBox" type="checkBox" required>';
+    }
+}
+randomCheckboxesNumber();
+
+function tickCheckBox(element) {
+    element.checked = true;
+}
+function randomTickCheckBoxes() {
+    const checkBoxes = document.getElementsByClassName("checkBox");
+    for (i = 0; i < checkBoxes.length; ++i) {
+        const random = Math.random();
+        if (random >= 0.5) {
+            tickCheckBox(checkBoxes[i]);
+        }
+    }
+    checkBoxes[Math.floor(Math.random() * checkBoxes.length)].checked = false;
+}
+randomTickCheckBoxes();
+
+document.querySelector(".submitButton").onclick = errorCheck;
+function errorCheck() {
+    const checkBoxes = document.getElementsByClassName("checkBox");
+    for (i = 0; i < checkBoxes.length; ++i) {
+        if (checkBoxes[i].checked !== true) {
+            event.preventDefault();
+            document.querySelector('.errorMessage').style.display = "block";
+        }
+    }
+}
