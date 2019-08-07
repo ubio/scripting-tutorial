ee1f1ee7b4df9ddb3f73ea3fe9546d0f42fd346b
andrei <andrei.marin@ub.io>
2019-08-07 10:18:52 +0300

    made requested changes (removed `pattern` property and moved constant `taskRowNumber` inside function `verifyAndHandle`)

--- tutorial/iterating-in-scope.js
+++ tutorial/iterating-in-scope.js
@@ -2,18 +2,19 @@ function randomTaskRowsAndNumber() {
     const randomNumber = Math.ceil(Math.random() * 3 + 2);
     for (i = 0; i < randomNumber; ++i) {
         const textToCopy = Math.random().toString().slice(0, 11);
-        document.querySelector('#taskRows').innerHTML += `<div class="row"><div class="textToCopyContainer"><p class="textToCopy">${textToCopy}</p></div><div class="inputField"><input class="inputFormField" type="text" name="inputText" required pattern="${textToCopy}"></div></div></br><div class="errorMessage badge badge--red--outline">Error: Please input requested text</br></div>`;
+        document.querySelector('#taskRows').innerHTML += `<div class="row"><div class="textToCopyContainer"><p class="textToCopy">${textToCopy}</p></div><div class="inputField"><input class="inputFormField" type="text" name="inputText" required></div></div></br><div class="errorMessage badge badge--red--outline">Error: Please input requested text</br></div>`;
     }
  }
 randomTaskRowsAndNumber();
 
-const taskRowsNumber = document.querySelectorAll('.row').length;
 document.querySelector('[name=submitButton]').onclick = verifyAndHandle;
 
 function verifyAndHandle () {
+    const taskRowsNumber = document.querySelectorAll('.row').length;
     for (i = 0; i < taskRowsNumber; ++i) {
-        const pattern = document.querySelectorAll('[name=inputText]')[i].pattern;
+        const pattern = document.querySelectorAll('.textToCopy')[i].innerText;
         const inputText = document.getElementsByClassName("inputFormField")[i].value;
+        document.getElementsByClassName("errorMessage")[i].style.display = "none";
         if (pattern !== inputText) {
             event.preventDefault();
             document.getElementsByClassName("errorMessage")[i].style.display = "block";
